0d75c643e90a4154d48cd5532aa35575a2958f0f
David Styrbjörn <davidstyrbjorn@gmail.com>
2019-04-24 14:17:07 +0200

    Added post processing scripts

--- Assets/Scripts/Utility/Pause.cs
+++ Assets/Scripts/Utility/Pause.cs
@@ -6,6 +6,7 @@ public class Pause : MonoBehaviour
 {
     public GameObject pause_object;
     private bool is_paused = false;
+    public UnityEngine.PostProcessing.PostProcessingProfile x;
 
     private void Update(){
         if(Input.GetKeyDown(KeyCode.Escape)){
@@ -13,10 +14,12 @@ public class Pause : MonoBehaviour
             if(is_paused){
                 pause_object.SetActive(true);
                 Time.timeScale = 0.0f;
+                x.grain.enabled = true;
             }
             else{
                 pause_object.SetActive(false);
                 Time.timeScale = 1.0f;
+                x.grain.enabled = false;
             }
         }
     }
