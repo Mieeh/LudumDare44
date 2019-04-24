fcdbe279f288ef634a166f55c7c06a20467a56ee
David Styrbjörn <davidstyrbjorn@gmail.com>
2019-04-24 11:11:01 +0200

    pause implemented

---
+++ Assets/Scripts/Utility/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float follow_speed = 1.0f;
+
+    private void Update(){
+        if(transform.position != target.position) {
+            transform.position = Vector3.Lerp(transform.position, target.position, follow_speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Utility/Pause.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pause_object;
+    private bool is_paused = false;
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            is_paused = !is_paused;
+            if(is_paused){
+                pause_object.SetActive(true);
+                Time.timeScale = 0.0f;
+            }
+            else{
+                pause_object.SetActive(false);
+                Time.timeScale = 1.0f;
+            }
+        }
+    }
+}
