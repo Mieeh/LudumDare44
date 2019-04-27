f4ed5a91aa4f04c8d1e326abea8591935b2f02c3
RobotGandhi <99viktor.vc@gmail.com>
2019-04-27 10:29:38 +0200

    We movin

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,7 +6,7 @@ public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float moveSpeed = 1.0f;
-    float verticalInput, horizontalInput;
+    float verticalInput, horizontalInput, verticalSpeed, horizontalSpeed;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -24,6 +24,8 @@ public class PlayerMove : MonoBehaviour
     }
 
     void Move(){
-        rb.velocity = new Vector2(horizontalInput, verticalInput) * moveSpeed;
+        verticalSpeed = Mathf.Lerp(0, verticalInput, 0.9f);
+        horizontalSpeed = Mathf.Lerp(0, horizontalInput, 0.9f);
+        rb.velocity = new Vector2(horizontalSpeed, verticalSpeed) * moveSpeed;
     }
 }
