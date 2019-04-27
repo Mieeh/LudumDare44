77e2d6aeca728b5b4bff60e44d0fcaf055ae1087
RobotGandhi <99viktor.vc@gmail.com>
2019-04-27 11:52:42 +0200

    I don't think you know how fast I really am.
    I'm fast as fuck, boi

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float moveSpeed = 1.0f;
+    private bool isSprinting, isMoving;
+    private float moveSpeed = 2.0f;
+    private float sprintSpeed = 3.0f;
     float verticalInput, horizontalInput, verticalSpeed, horizontalSpeed;
+    private KeyCode dashDirection, potentialDashDirection;
+    private ArrayList keysToCheck;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
         Application.targetFrameRate = 60;
+        isSprinting = false;
+        isMoving = false;
+
+        keysToCheck = new ArrayList();
+        keysToCheck.Add(KeyCode.W);
+        keysToCheck.Add(KeyCode.A);
+        keysToCheck.Add(KeyCode.S);
+        keysToCheck.Add(KeyCode.D);
     }
 
     private void Update() {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        //If the player presses a key during the frame, they are considered to be moving.
+        //A check for wether or not the same button is pushed twice is used to determine player sprinting.
+        //The sprint stops when the player stops moving.
+        foreach (KeyCode key in keysToCheck) {
+            if (Input.GetKeyUp(key)) {
+                if (key == dashDirection) {
+                    isSprinting = false;
+                }
+                potentialDashDirection = key;
+                StartCoroutine("DashCheck");
+            }
+            if (Input.GetKey(key)) {
+                isMoving = true;
+            }
+        }
+        //Debug.Log(isMoving);
+        if (!isMoving) {
+            isSprinting = false;
+        }
+        isMoving = false;
     }
 
     private void FixedUpdate() {
@@ -23,9 +56,23 @@ public class PlayerMove : MonoBehaviour
         Move();
     }
 
+    IEnumerator DashCheck() {
+        yield return new WaitForSeconds(0.25f);
+        if (Input.GetKey(potentialDashDirection)) {
+            dashDirection = potentialDashDirection;
+            isSprinting = true;
+        }
+    }
+
     void Move(){
+        Debug.Log(isSprinting);
         verticalSpeed = Mathf.Lerp(0, verticalInput, 0.9f);
         horizontalSpeed = Mathf.Lerp(0, horizontalInput, 0.9f);
-        rb.velocity = new Vector2(horizontalSpeed, verticalSpeed) * moveSpeed;
+
+        float currentSpeed;
+        if (isSprinting) currentSpeed = sprintSpeed;
+        else currentSpeed = moveSpeed;
+
+        rb.velocity = new Vector2(horizontalSpeed, verticalSpeed) * currentSpeed;
     }
 }
