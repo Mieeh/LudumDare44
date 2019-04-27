90f8f31cf9f1ed46bca1fb7219e713a39d235d2e
RobotGandhi <99viktor.vc@gmail.com>
2019-04-27 14:43:29 +0200

    Keep rollin' rollin' rollin'

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public bool isSprinting, isMoving;
+    public bool isSprinting, isMoving, isDodging, isInvincible;
     private float moveSpeed = 2.0f;
     private float sprintSpeed = 3.0f;
-    float verticalInput, horizontalInput, verticalSpeed, horizontalSpeed;
+    private float invincibilityTimeInSeconds = 0.7f;
+    private float dodgeTimeInSeconds = 1.0f;
+    float verticalInput, horizontalInput, verticalSpeed, horizontalSpeed, timeElapsed;
     private KeyCode dashDirection, potentialDashDirection;
-    private ArrayList keysToCheck;
+    private ArrayList movementKeys;
+    private Vector2 dodgeDirection;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
-        Application.targetFrameRate = 60;
+        //Application.targetFrameRate = 60;
         isSprinting = false;
         isMoving = false;
 
-        keysToCheck = new ArrayList();
-        keysToCheck.Add(KeyCode.W);
-        keysToCheck.Add(KeyCode.A);
-        keysToCheck.Add(KeyCode.S);
-        keysToCheck.Add(KeyCode.D);
+        movementKeys = new ArrayList();
+        movementKeys.Add(KeyCode.W);
+        movementKeys.Add(KeyCode.A);
+        movementKeys.Add(KeyCode.S);
+        movementKeys.Add(KeyCode.D);
     }
 
     private void Update() {
@@ -32,7 +35,7 @@ public class PlayerMove : MonoBehaviour
         //If the player presses a key during the frame, they are considered to be moving.
         //A check for wether or not the same button is pushed twice is used to determine player sprinting.
         //The sprint stops when the player stops moving.
-        foreach (KeyCode key in keysToCheck) {
+        foreach (KeyCode key in movementKeys) {
             if (Input.GetKeyUp(key)) {
                 if (key == dashDirection) {
                     isSprinting = false;
@@ -49,6 +52,17 @@ public class PlayerMove : MonoBehaviour
             isSprinting = false;
         }
         isMoving = false;
+
+        if (Input.GetKeyDown(KeyCode.K) && !isDodging) {
+            if (horizontalInput != 0 || verticalInput != 0) {
+                dodgeDirection.x = Mathf.Lerp(0, horizontalInput, 0.9f);
+                dodgeDirection.y = Mathf.Lerp(0, verticalInput, 0.9f);
+                timeElapsed = 0.0f;
+                GetComponent<Collider2D>().enabled = false;
+                StartCoroutine("DodgeTimer");
+                StartCoroutine("InvincibilityTimer");
+            }
+        }
     }
 
     private void FixedUpdate() {
@@ -64,15 +78,33 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
+    IEnumerator InvincibilityTimer() {
+        isInvincible = true;
+        yield return new WaitForSeconds(invincibilityTimeInSeconds);
+        isInvincible = false;
+        GetComponent<Collider2D>().enabled = true;
+    }
+    IEnumerator DodgeTimer() {
+        isDodging = true;
+        yield return new WaitForSeconds(dodgeTimeInSeconds);
+        isDodging = false;
+    }
+
     void Move(){
-        Debug.Log(isSprinting);
-        verticalSpeed = Mathf.Lerp(0, verticalInput, 0.9f);
-        horizontalSpeed = Mathf.Lerp(0, horizontalInput, 0.9f);
+        if (isDodging) {
+            timeElapsed += Time.deltaTime;
+            rb.velocity = dodgeDirection * (moveSpeed / Mathf.Pow(timeElapsed + 0.3f, 2) / dodgeDirection.magnitude);
+            //rb.velocity = Vector2.ClampMagnitude(rb.velocity, 17.6f);
+            Debug.Log(rb.velocity.magnitude);
+        } else {
+            horizontalSpeed = Mathf.Lerp(0, horizontalInput, 0.9f);
+            verticalSpeed = Mathf.Lerp(0, verticalInput, 0.9f);
 
-        float currentSpeed;
-        if (isSprinting) currentSpeed = sprintSpeed;
-        else currentSpeed = moveSpeed;
+            float currentSpeed;
+            if (isSprinting) currentSpeed = sprintSpeed;
+            else currentSpeed = moveSpeed;
 
-        rb.velocity = new Vector2(horizontalSpeed, verticalSpeed) * currentSpeed;
+            rb.velocity = new Vector2(horizontalSpeed, verticalSpeed) * currentSpeed;
+        }
     }
 }
